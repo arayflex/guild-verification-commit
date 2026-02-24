36e3568fc991057fc40b58ffb989b2f134332b95
arayflex <ariferdemaydin@gmail.com>
2026-02-24 03:17:50 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-24T00:17:38.381Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-02-24T00:17:49.831Z
+Commit: Fix broken links in documentation

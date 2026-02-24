34c168ed27afa1478305024475bfe9b4903e4c75
arayflex <ariferdemaydin@gmail.com>
2026-02-24 03:15:09 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-24T00:14:38.518Z
-Commit: Implement user profile customization
+Timestamp: 2026-02-24T00:15:08.866Z
+Commit: Fix memory leak in event handlers

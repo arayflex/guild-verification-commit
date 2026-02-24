d400639264c866d43a3a156ce4ae9ed2812ae21e
arayflex <ariferdemaydin@gmail.com>
2026-02-24 03:17:37 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-24T00:17:31.868Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-02-24T00:17:37.379Z
+Commit: Fix broken links in documentation

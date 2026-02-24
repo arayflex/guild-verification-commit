d338c3074c1f103eaf0867549ae89504be3e86a0
arayflex <ariferdemaydin@gmail.com>
2026-02-24 03:18:31 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-24T00:18:30.099Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-02-24T00:18:31.014Z
+Commit: Fix broken links in documentation

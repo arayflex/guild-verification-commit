f9feadc0e1b38008aba1992028fd44de08a34f15
arayflex <ariferdemaydin@gmail.com>
2026-02-24 03:15:15 +0300

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-24T00:15:02.583Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-02-24T00:15:15.534Z
+Commit: Add export functionality for reports
